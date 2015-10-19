6e292bb07d57e2b0d8250c091b2ef1dc4fdb9047
maxLoveCode <maxkidy@gmail.com>
2015-10-19 15:11:06 +0800

    ide commit

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
 class User(models.Model):
-	name = models.CharField(max_length = 10)
-	def __str__(self):
-		return self.name
-	
+    name = models.CharField(max_length=20)
+    avatar = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Works(models.Model):
- 	artist = models.ForeignKey(User)
- 	title = models.CharField(max_length = 10)
- 	time = models.DateTimeField('date published')
- 	def __str__(self):
- 		return self.artist.title
+    artist = models.ForeignKey(User)
+    title = models.CharField(max_length=10, default='DEFAULT TITLE')
+    time = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.artist.title
