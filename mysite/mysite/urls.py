a154c28efbdc59bfed69701a809440156d5b9b57
maxLoveCode <maxkidy@gmail.com>
2015-10-27 15:38:39 +0800

    font

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'home.views.index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^users/', include('users.urls')),
 ]
