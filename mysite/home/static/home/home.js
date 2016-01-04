4d395efe3252f93253ca7640813bdabbf2cae407
maxLoveCode <maxkidy@gmail.com>
2016-01-04 18:09:15 +0800

    ide commit

--- mysite/home/static/home/home.js
+++ mysite/home/static/home/home.js
@@ -1,4 +1,19 @@
-$("#video1").bind('ended', function(){
-    window.alert(3+3);
-    this.play();
+$(document).ready(function(){
+    $(".tagline").click(function(){
+
+        $(".fullscreen-bg__video").play();
+        alert("play");
+    });
+
+    $(".fullscreen-bg__video").bind('ended', function(){
+        alert("ended")
+        this.play();
+    });
+
+    $(".fullscreen-bg__video").bind('play', function(){
+    });
+
+    $(".fullscreen-bg__video").click(function(){
+    });
 });
+
