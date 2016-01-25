bc98ffa8d05b360e10f8f15e48b318cd7266f1d6
maxLoveCode <maxkidy@gmail.com>
2016-01-25 10:34:00 +0800

    jan changes

--- mysite/cvproject/urls.py
+++ mysite/cvproject/urls.py
@@ -1,11 +1,14 @@
 __author__ = 'max ma'
 
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
